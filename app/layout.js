4397aae27698a7f3ee02908a543863f3d8fa88a0
Shinahin <tailangrongyi@gmail.com>
2024-11-08 15:36:35 +0900

    update gtag

--- app/layout.js
+++ app/layout.js
@@ -9,8 +9,8 @@ import { GoogleAnalytics } from '@next/third-parties/google'
 export default function RootLayout({ children }) {
   return (
     <html lang="ja">
-      <GoogleAnalytics gaId="G-XD9P316J50" />
       <body className="dark:bg-slate-800 dark:text-white">
+        <GoogleAnalytics gaId="G-XD9P316J50" />
         {children}
       </body>
     </html>
