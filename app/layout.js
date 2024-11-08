467a4ec4fe2985829b309e63f4a3ec20f6cac694
Shinahin <tailangrongyi@gmail.com>
2024-11-08 15:42:17 +0900

    update script

--- app/layout.js
+++ app/layout.js
@@ -5,12 +5,14 @@ export const metadata = {
   description: 'たよりさんのホームページのホーム',
 };
 import { GoogleAnalytics } from '@next/third-parties/google'
+import Script from 'next/script'
 
 export default function RootLayout({ children }) {
   return (
     <html lang="ja">
       <body className="dark:bg-slate-800 dark:text-white">
         <GoogleAnalytics gaId="G-XD9P316J50" />
+        <script type="text/javascript" data-cmp-ab="1" src="https://cdn.consentmanager.net/delivery/autoblocking/5aa44bce21b5d.js" data-cmp-host="b.delivery.consentmanager.net" data-cmp-cdn="cdn.consentmanager.net" data-cmp-codesrc="16"></script>
         {children}
       </body>
     </html>
