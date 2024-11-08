b8c7796d80882fd3afa628469ae96684c60dedf6
Shinahin <tailangrongyi@gmail.com>
2024-11-08 15:30:03 +0900

    update g

--- app/layout.js
+++ app/layout.js
@@ -4,10 +4,12 @@ export const metadata = {
   title: 'ホーム',
   description: 'たよりさんのホームページのホーム',
 };
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 export default function RootLayout({ children }) {
   return (
     <html lang="ja">
+      <GoogleAnalytics gaId="G-XD9P316J50" />
       <body className="dark:bg-slate-800 dark:text-white">
         {children}
       </body>
